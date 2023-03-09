45d969324509d2ebb2f455580cec09b35e26fd61
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-03-09 15:57:08 +0100

    Add new application swh-loader-metadata
    Refs. swh/infra/sysadm-environment#4794

---
+++ apps/swh-loader-metadata/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "${SWH_CONFIG_FILENAME}" ]; then
+  echo "The SWH_CONFIG_FILENAME environment variable must be set"
+  exit 1
+fi
+
+if [ ! -e "${SWH_CONFIG_FILENAME}" ]; then
+  echo "The config file ${SWH_CONFIG_FILENAME} does not exist"
+  exit 1
+fi
+
+echo "Starting loader metadata journal client..."
+exec swh \
+  --log-level $LOGLEVEL \
+  loader metadata \
+  --config-file $SWH_CONFIG_FILENAME \
+  journal-client $SWH_INDEXER_TYPE
