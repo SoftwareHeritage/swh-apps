7f6d442e7827ac6334c3386d12890f39f6c37c2a
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-10-06 15:22:20 +0200

    toolbox: Improve the welcome message

--- apps/swh-toolbox/entrypoint.sh
+++ apps/swh-toolbox/entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-MSG='USAGE_MSG="\e[1;31mBeware of SWH_CONFIG_FILENAME variable !\n
-This variable may change according to the context that you use this toolbox.\n
-You must define this variable by yourself.\e[m\n\n
-\e[0;33mFor example to process add-forge-now request in staging:\e[m\n
-export SWH_CONFIG_FILENAME=/etc/swh/config-scheduler-staging.yml\n"'
+MSG='USAGE_MSG="\e[1;31mSWH_CONFIG_FILENAME variable is not set!\n\n
+This variable must be defined according to your use case (e.g. .\n
+scheduler, storage, vault, ...). You must define it by yourself.\e[m\n\n
+\e[0;33mFor example to process add-forge-now requests:\e[m\n
+export SWH_CONFIG_FILENAME=/etc/swh/config-scheduler.yml\n"'
 echo $MSG >> .bashrc
 echo 'echo -e $USAGE_MSG' >> .bashrc
 
