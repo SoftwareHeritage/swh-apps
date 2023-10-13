6038f484d37635e5003f4a26f6982a04baa11f85
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-10-13 12:34:11 +0200

    Create the indexer storage image
    Refs. swh/infra/sysadm-environment#4780

---
+++ apps/swh-indexer-storage/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+case "$1" in
+    "shell")
+      exec bash -i
+      ;;
+    "swh")
+        shift
+        echo "Running swh command $@"
+        exec swh $@
+        ;;
+    *)
+      echo Starting the swh-indexer-storage API server
+      exec gunicorn --bind 0.0.0.0:${PORT} \
+           --threads ${THREADS} \
+           --workers ${WORKERS} \
+           --log-level ${LOG_LEVEL} \
+           --timeout ${TIMEOUT} \
+           --config 'python:swh.core.api.gunicorn_config' \
+           'swh.indexer.storage.api.server:make_app_from_configfile()'
+      ;;
+esac
