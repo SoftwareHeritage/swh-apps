cafbe5bb679105f5c9b83c95011492e89b3f367f
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2023-10-13 14:53:36 +0200

    Add swh-search application
    Refs. swh/infra/sysadm-environment#4780

---
+++ apps/swh-search/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+case "$1" in
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
+        fi
+        ;;
+    "swh")
+        shift
+        echo "Running swh command $@"
+        exec swh $@
+        ;;
+    *)
+        echo Starting the swh-scheduler API server
+        exec gunicorn --bind 0.0.0.0:${PORT} \
+            --log-level ${LOG_LEVEL} \
+            --threads ${THREADS} \
+            --workers ${WORKERS} \
+            --reload \
+            --timeout ${TIMEOUT} \
+            --config 'python:swh.core.api.gunicorn_config' \
+            'swh.search.api.server:make_app_from_configfile()'
+esac
