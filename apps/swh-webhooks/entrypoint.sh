faafdb58653996b20e0fd74d55aaa97c15fa25e9
Guillaume Samson <guillaume.samson@softwareheritage.org>
2023-11-28 12:11:39 +0100

    swh-webhooks: Create the image

---
+++ apps/swh-webhooks/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+case "$1" in
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
+        fi
+        ;;
+    "swh")
+        shift
+        echo "Running swh command $@"
+        exec swh $@
+        ;;
+    *)
+        echo "Start swh-webhooks journal-client"
+        exec swh webhooks journal-client
+esac
