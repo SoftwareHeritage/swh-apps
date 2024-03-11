9e737b4c85ca546e2ac8c31d015f082aad2ee270
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2024-03-11 18:06:30 +0100

    cassandra-checks: Allow update of the snippets folder at startup
    So we can have fix when restarting journal clients.
    
    Refs. swh/infra/sysadm-environment#4707

--- apps/cassandra-checks/entrypoint.sh
+++ apps/cassandra-checks/entrypoint.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+CLONE_DIR=/opt/swh/snippets
+
+if [ ! -d $CLONE_DIR ]; then
+    git clone --depth=1 https://gitlab.softwareheritage.org/swh/devel/snippets.git $CLONE_DIR
+else
+    pushd $CLONE_DIR
+    git fetch
+    git merge origin/master
+    popd
+fi
+
 case "$1" in
   "shell")
     shift
@@ -10,6 +21,6 @@ case "$1" in
     ;;
   *)
     echo Starting Cassandra objects checker.
-    exec /opt/swh/snippets/sysadmin/cassandra_checks/get_journal_check_and_replay.py
+    exec $CLONE_DIR/sysadmin/cassandra_checks/get_journal_check_and_replay.py
     ;;
 esac
