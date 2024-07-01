4fece45e08cf4a1ae8ce1e3b6b51a53d7146fa6c
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2024-07-01 17:24:07 +0200

    toolbox: Improve toolbox welcome message

--- apps/swh-toolbox/entrypoint.sh
+++ apps/swh-toolbox/entrypoint.sh
@@ -5,10 +5,11 @@ set -e
 MSG='USAGE_MSG="\e[1;31mSWH_CONFIG_FILENAME variable is not set!\n\n
 This variable must be defined according to your use case (e.g. .\n
 scheduler, storage, vault, ...). You must define it by yourself.\e[m\n\n
-\e[0;33mFor example to process add-forge-now requests:\e[m\n
-export SWH_CONFIG_FILENAME=/etc/swh/config-scheduler.yml\n"'
+\e[0;33mFor example, use one of the following:\e[m\n"'
+
 echo $MSG >> .bashrc
 echo 'echo -e $USAGE_MSG' >> .bashrc
+echo "find /etc/swh/ -type f -iname '*.yml' | xargs -i echo 'export SWH_CONFIG_FILENAME={}'" >> .bashrc
 
 echo "Administration purpose pod: connect to it and do what you will!"
 while sleep infinity; do :; done
