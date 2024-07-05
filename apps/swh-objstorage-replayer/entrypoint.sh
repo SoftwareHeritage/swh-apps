58680083b4c5506b8ff9456e5aa543f1c5e988b9
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2024-07-05 14:36:13 +0200

    *-replayer/entrypoint.sh: Fix typos

--- apps/swh-objstorage-replayer/entrypoint.sh
+++ apps/swh-objstorage-replayer/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ -z "${SWH_CONFIG_FILENAME}" ]; then
-  echo "The SWH_CONFIG_FILENAME" environment must be set
+  echo "The SWH_CONFIG_FILENAME environment variable must be set"
   exit 1
 fi
 
 if [ ! -e "${SWH_CONFIG_FILENAME}" ]; then
-  echo "The config file ${SWH_CONFIG_FILENAME} does not exists"
+  echo "The config file ${SWH_CONFIG_FILENAME} does not exist"
   exit 1
 fi
 
