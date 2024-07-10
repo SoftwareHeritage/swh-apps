1ef61c08010c62f5ef0b3f9dc62a281ee8de9f8a
Nicolas Dandrimont <nicolas@dandrimont.eu>
2024-07-10 17:24:53 +0200

    swh-alter: run tmux in a way that is compatible with bullseye

--- apps/swh-alter/entrypoint.sh
+++ apps/swh-alter/entrypoint.sh
@@ -3,4 +3,9 @@
 set -e
 
 echo "swh-alter pod: running a tmux server"
-exec tmux -D
+tmux set -g exit-empty off \; start
+
+trap "tmux kill-server" INT TERM EXIT
+
+while tmux list-sessions 2>/dev/null; do sleep 5; done
+echo "tmux server has exited, bailing"
