3a754a50feaade052f2ba390cda36e936b9c7d0c
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2024-08-14 11:49:46 +0200

    counters/entrypoint: Indent shell

--- apps/swh-counters/entrypoint.sh
+++ apps/swh-counters/entrypoint.sh
@@ -19,13 +19,13 @@ case "$1" in
     *)
         echo Starting the swh-search API server
         exec gunicorn --bind 0.0.0.0:${PORT} \
-            --log-level ${SWH_LOG_LEVEL:-INFO} \
-            --threads ${THREADS} \
-            --workers ${WORKERS} \
-            --timeout ${TIMEOUT} \
-            --statsd-host=${STATSD_HOST}:${STATSD_PORT} \
-            --statsd-prefix=${STATSD_SERVICE_TYPE} \
-            --reload \
-            --config 'python:swh.core.api.gunicorn_config' \
-            'swh.counters.api.server:make_app_from_configfile()'
+             --log-level ${SWH_LOG_LEVEL:-INFO} \
+             --threads ${THREADS} \
+             --workers ${WORKERS} \
+             --timeout ${TIMEOUT} \
+             --statsd-host=${STATSD_HOST}:${STATSD_PORT} \
+             --statsd-prefix=${STATSD_SERVICE_TYPE} \
+             --reload \
+             --config 'python:swh.core.api.gunicorn_config' \
+             'swh.counters.api.server:make_app_from_configfile()'
 esac
