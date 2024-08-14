15078ba14016ddfd84c10d478013d8a0eb12ded4
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2024-08-14 14:01:34 +0200

    deposit/entrypoint: Fix shell indent

--- apps/swh-deposit/entrypoint.sh
+++ apps/swh-deposit/entrypoint.sh
@@ -19,14 +19,14 @@ case "$1" in
     *)
         echo Starting the swh-deposit API server
         exec gunicorn --bind 0.0.0.0:${PORT} \
-            --log-level ${SWH_LOG_LEVEL:-INFO} \
-            --threads ${THREADS} \
-            --workers ${WORKERS} \
-            --timeout ${TIMEOUT} \
-            --statsd-host=${STATSD_HOST}:${STATSD_PORT} \
-            --statsd-prefix=${STATSD_SERVICE_TYPE} \
-            --reload \
-            --env DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE} \
-            --config 'python:swh.core.api.gunicorn_config' \
-            'django.core.wsgi:get_wsgi_application()'
+             --log-level ${SWH_LOG_LEVEL:-INFO} \
+             --threads ${THREADS} \
+             --workers ${WORKERS} \
+             --timeout ${TIMEOUT} \
+             --statsd-host=${STATSD_HOST}:${STATSD_PORT} \
+             --statsd-prefix=${STATSD_SERVICE_TYPE} \
+             --reload \
+             --env DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE} \
+             --config 'python:swh.core.api.gunicorn_config' \
+             'django.core.wsgi:get_wsgi_application()'
 esac
