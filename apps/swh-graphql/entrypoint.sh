d8c8f9166dfa34d340bf2c49a091398257fbce8e
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2024-08-14 13:24:04 +0200

    graphql/entrypoint: Fix shell indent

--- apps/swh-graphql/entrypoint.sh
+++ apps/swh-graphql/entrypoint.sh
@@ -3,21 +3,20 @@
 set -e
 
 case "$1" in
-"shell")
-  exec bash -i
-  ;;
-*)
-  echo Starting the swh-graphql API server
-
-  exec gunicorn --bind 0.0.0.0:${PORT} \
-    --log-level ${SWH_LOG_LEVEL:-INFO} \
-    --threads ${THREADS} \
-    --workers ${WORKERS} \
-    --timeout ${TIMEOUT} \
-    --statsd-host=${STATSD_HOST}:${STATSD_PORT} \
-    --statsd-prefix=${STATSD_SERVICE_TYPE} \
-    --config 'python:swh.graphql.gunicorn_config' \
-    --worker-class 'uvicorn.workers.UvicornWorker' \
-    'swh.graphql.server:make_app_from_configfile()'
-  ;;
+    "shell")
+        exec bash -i
+        ;;
+    *)
+        echo Starting the swh-graphql API server
+        exec gunicorn --bind 0.0.0.0:${PORT} \
+             --log-level ${SWH_LOG_LEVEL:-INFO} \
+             --threads ${THREADS} \
+             --workers ${WORKERS} \
+             --timeout ${TIMEOUT} \
+             --statsd-host=${STATSD_HOST}:${STATSD_PORT} \
+             --statsd-prefix=${STATSD_SERVICE_TYPE} \
+             --config 'python:swh.graphql.gunicorn_config' \
+             --worker-class 'uvicorn.workers.UvicornWorker' \
+             'swh.graphql.server:make_app_from_configfile()'
+        ;;
 esac
