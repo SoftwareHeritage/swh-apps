dda820b44b57400cd4e8a7c039fe5459d4716d23
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2024-08-14 11:01:13 +0200

    objstorage/entrypoint: Indent shell

--- apps/swh-indexer-storage/entrypoint.sh
+++ apps/swh-indexer-storage/entrypoint.sh
@@ -4,23 +4,23 @@ set -e
 
 case "$1" in
     "shell")
-      exec bash -i
-      ;;
+        exec bash -i
+        ;;
     "swh")
         shift
         echo "Running swh command $@"
         exec swh $@
         ;;
     *)
-      echo Starting the swh-indexer-storage API server
-      exec gunicorn --bind 0.0.0.0:${PORT} \
-           --log-level ${SWH_LOG_LEVEL:-INFO} \
-           --threads ${THREADS} \
-           --workers ${WORKERS} \
-           --timeout ${TIMEOUT} \
-           --statsd-host=${STATSD_HOST}:${STATSD_PORT} \
-           --statsd-prefix=${STATSD_SERVICE_TYPE} \
-           --config 'python:swh.core.api.gunicorn_config' \
-           'swh.indexer.storage.api.server:make_app_from_configfile()'
-      ;;
+        echo Starting the swh-indexer-storage API server
+        exec gunicorn --bind 0.0.0.0:${PORT} \
+             --log-level ${SWH_LOG_LEVEL:-INFO} \
+             --threads ${THREADS} \
+             --workers ${WORKERS} \
+             --timeout ${TIMEOUT} \
+             --statsd-host=${STATSD_HOST}:${STATSD_PORT} \
+             --statsd-prefix=${STATSD_SERVICE_TYPE} \
+             --config 'python:swh.core.api.gunicorn_config' \
+             'swh.indexer.storage.api.server:make_app_from_configfile()'
+        ;;
 esac
