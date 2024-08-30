25181f2839687459c727608811d08404df96489d
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2024-08-30 09:21:43 +0200

    Add provenance image
    Refs. swh/infra/sysadm-environment#5398

---
+++ apps/swh-provenance/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+case "$1" in
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
+        fi
+        ;;
+    "swh")
+        shift
+        echo "Running swh command $@"
+        exec swh $@
+        ;;
+    *)
+        EXTRA_CLI_FLAGS=""
+        if [ ! -z "${SWH_LOG_CONFIG_JSON}" ]; then
+            EXTRA_CLI_FLAGS="--log-config-json ${SWH_LOG_CONFIG_JSON}"
+        fi
+        echo Starting the swh-provenance RPC server
+        exec gunicorn --bind 0.0.0.0:${PORT} \
+             --log-level ${SWH_LOG_LEVEL:-INFO} \
+             $EXTRA_CLI_FLAGS \
+             --threads ${THREADS} \
+             --workers ${WORKERS} \
+             --timeout ${TIMEOUT} \
+             --reload \
+             --statsd-host=${STATSD_HOST}:${STATSD_PORT} \
+             --statsd-prefix=${STATSD_SERVICE_TYPE} \
+             --config 'python:swh.core.api.gunicorn_config' \
+             'swh.provenance.api.server:make_app_from_configfile()'
+      ;;
+esac
