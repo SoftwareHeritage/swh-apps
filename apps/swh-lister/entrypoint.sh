a4b0cfa6565c8865d65f899cd7d4d448d0b0c121
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-02-01 17:39:01 +0100

    swh-lister: Unify entrypoint copy and format
    It's copied at the end of the Dockerfile so modification to it do not rebuild
    deps. The entrypoint script is also adapted to work with actual docker run
    calls.

--- apps/swh-lister/entrypoint.sh
+++ apps/swh-lister/entrypoint.sh
@@ -3,22 +3,25 @@
 set -e
 
 case "$1" in
-  "shell")
-    shift
-    echo "Running command $@"
-    exec bash -i "$@"
-    ;;
-  *)
-    echo Starting the swh Celery worker for ${SWH_WORKER_INSTANCE}
-    exec python -m celery \
-       --app=swh.scheduler.celery_backend.config.app \
-       worker \
-       --pool=prefork --events \
-       --concurrency=${CONCURRENCY} \
-       --max-tasks-per-child=${MAX_TASKS_PER_CHILD} \
-       -Ofair \
-       --loglevel=${SWH_LOG_LEVEL:-INFO} \
-       --without-gossip --without-mingle --without-heartbeat \
-       --hostname "${SWH_WORKER_INSTANCE}@%h"
-    ;;
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
+        fi
+        ;;
+    *)
+        echo Starting the swh Celery worker for ${SWH_WORKER_INSTANCE}
+        exec python -m celery \
+             --app=swh.scheduler.celery_backend.config.app \
+             worker \
+             --pool=prefork --events \
+             --concurrency=${CONCURRENCY} \
+             --max-tasks-per-child=${MAX_TASKS_PER_CHILD} \
+             -Ofair \
+             --loglevel=${SWH_LOG_LEVEL:-INFO} \
+             --without-gossip --without-mingle --without-heartbeat \
+             --hostname "${SWH_WORKER_INSTANCE}@%h"
+        ;;
 esac
