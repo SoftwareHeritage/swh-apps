2a0f8d76a20cc59cc3e2dde4c87d5b5fc749a995
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-02-01 17:48:44 +0100

    swh-loader-package: Unify entrypoint copy and format
    It's copied at the end of the Dockerfile so modification to it do not rebuild
    deps. The entrypoint script is also adapted to work with actual docker run
    calls.

--- apps/swh-loader-package/entrypoint.sh
+++ apps/swh-loader-package/entrypoint.sh
@@ -2,28 +2,31 @@
 
 set -e
 
-# If a pod is killed by the OOM killer, the ephemeral volumes
-# are not recreated so it can remain the previous work dir.
-echo "Cleaning /tmp..."
-rm -rf /tmp/*
-
 case "$1" in
-  "shell")
-    shift
-    echo "Running command $@"
-    exec bash -i "$@"
-    ;;
-  *)
-    echo Starting the swh Celery worker for ${SWH_WORKER_INSTANCE}
-    exec python -m celery \
-       --app=swh.scheduler.celery_backend.config.app \
-       worker \
-       --pool=prefork \
-       --concurrency=${CONCURRENCY} \
-       --max-tasks-per-child=${MAX_TASKS_PER_CHILD} \
-       -Ofair \
-       --loglevel=${SWH_LOG_LEVEL:-INFO} \
-       --without-gossip --without-mingle --without-heartbeat \
-       --hostname "${SWH_WORKER_INSTANCE}@%h"
-    ;;
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
+        fi
+        ;;
+    *)
+        # If a pod is killed by the OOM killer, there can be workdir leftover
+        # as the ephemeral volumes are not recreated.
+        echo "Cleaning /tmp..."
+        rm -rf /tmp/*
+
+        echo Starting the swh Celery worker for ${SWH_WORKER_INSTANCE}
+        exec python -m celery \
+             --app=swh.scheduler.celery_backend.config.app \
+             worker \
+             --pool=prefork \
+             --concurrency=${CONCURRENCY} \
+             --max-tasks-per-child=${MAX_TASKS_PER_CHILD} \
+             -Ofair \
+             --loglevel=${SWH_LOG_LEVEL:-INFO} \
+             --without-gossip --without-mingle --without-heartbeat \
+             --hostname "${SWH_WORKER_INSTANCE}@%h"
+        ;;
 esac
