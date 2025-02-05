7c7a6afc42c8b6f8f4e57e1bf73e682496aacd80
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-02-05 15:17:11 +0100

    graph/entrypoint: Allow to run the graph image
    This also aligns the entrypoint with the others.

--- apps/swh-graph/entrypoint.sh
+++ apps/swh-graph/entrypoint.sh
@@ -2,22 +2,34 @@
 
 set -e
 
-case "${GRAPH_TYPE}" in
-    "rpc")
-        subcmd="rpc-serve -h 0.0.0.0 -p ${PORT}"
-        if [ ! -z "${GRAPH_PATH}" ]; then
-           subcmd+=" -g ${GRAPH_PATH}"
+case "$1" in
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
         fi
-        ;;
-    "grpc")
-        subcmd="grpc-serve -g ${GRAPH_PATH} -p ${PORT}"
+        exit 0
         ;;
     *)
-        echo "Unknown graph type <$GRAPH_TYPE> (either rpc or gprc)"
-        exit 1
-esac
+        case "${GRAPH_TYPE}" in
+            "rpc")
+                subcmd="rpc-serve -h 0.0.0.0 -p ${PORT}"
+                if [ ! -z "${GRAPH_PATH}" ]; then
+                    subcmd+=" -g ${GRAPH_PATH}"
+                fi
+                ;;
+            "grpc")
+                subcmd="grpc-serve -g ${GRAPH_PATH} -p ${PORT}"
+                ;;
+            *)
+                echo "Unknown graph type <$GRAPH_TYPE> (either rpc or gprc)"
+                exit 1
+        esac
 
-echo "Starting the swh-graph ${GRAPH_TYPE} server"
-cmd="swh graph -C ${SWH_CONFIG_FILENAME} $subcmd"
-echo $cmd
-$cmd
+        echo "Starting the swh-graph ${GRAPH_TYPE} server"
+        cmd="swh graph -C ${SWH_CONFIG_FILENAME} $subcmd"
+        echo $cmd
+        $cmd
+esac
