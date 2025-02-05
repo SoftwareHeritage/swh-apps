7061abeb1d199fbe94d5b150a08648e128826216
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-02-05 15:59:13 +0100

    objstorage-replayer/entrypoint: Allow to run the image
    This also aligns the entrypoint with the others.

--- apps/swh-objstorage-replayer/entrypoint.sh
+++ apps/swh-objstorage-replayer/entrypoint.sh
@@ -1,39 +1,54 @@
 #!/bin/bash
 
-if [ -z "${SWH_CONFIG_FILENAME}" ]; then
-  echo "The SWH_CONFIG_FILENAME environment variable must be set"
-  exit 1
-fi
+set -e
 
-if [ ! -e "${SWH_CONFIG_FILENAME}" ]; then
-  echo "The config file ${SWH_CONFIG_FILENAME} does not exist"
-  exit 1
-fi
+case "$1" in
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
+        fi
+        exit 0
+        ;;
+    *)
+        if [ -z "${SWH_CONFIG_FILENAME}" ]; then
+            echo "The SWH_CONFIG_FILENAME environment variable must be set"
+            exit 1
+        fi
 
-OPTIONS=()
+        if [ ! -e "${SWH_CONFIG_FILENAME}" ]; then
+            echo "The config file ${SWH_CONFIG_FILENAME} does not exist"
+            exit 1
+        fi
 
-if [ "${CHECK_DST}" == "false" ]; then
-  OPTIONS+=("--no-check-dst")
-fi
+        OPTIONS=()
 
-if [ -n "${FETCH_CONCURRENCY}" ]; then
-  OPTIONS+=("--concurrency" "${FETCH_CONCURRENCY}")
-fi
+        if [ "${CHECK_DST}" == "false" ]; then
+            OPTIONS+=("--no-check-dst")
+        fi
 
-if [ -n "${STALL_REPORT_FILENAME}" ]; then
-  OPTIONS+=("--stall-report-filename" "${STALL_REPORT_FILENAME}")
-fi
+        if [ -n "${FETCH_CONCURRENCY}" ]; then
+            OPTIONS+=("--concurrency" "${FETCH_CONCURRENCY}")
+        fi
 
-if [ "${SET_KAFKA_GROUP_INSTANCE_ID_TO_HOSTNAME}" = "true" ]; then
-  KAFKA_GROUP_INSTANCE_ID="$(hostname)"
-  export KAFKA_GROUP_INSTANCE_ID
-fi
+        if [ -n "${STALL_REPORT_FILENAME}" ]; then
+            OPTIONS+=("--stall-report-filename" "${STALL_REPORT_FILENAME}")
+        fi
 
-# start the replayer
-echo "Starting the content replayer..."
-# word split $SWH_EXTRA_CLI_OPTIONS explicitly
-# shellcheck disable=SC2206
-CMD=("swh" $SWH_EXTRA_CLI_OPTIONS "objstorage" "replay" "${OPTIONS[@]}" "$@")
-echo "${CMD[@]}"
+        if [ "${SET_KAFKA_GROUP_INSTANCE_ID_TO_HOSTNAME}" = "true" ]; then
+            KAFKA_GROUP_INSTANCE_ID="$(hostname)"
+            export KAFKA_GROUP_INSTANCE_ID
+        fi
 
-exec "${CMD[@]}"
+        # start the replayer
+        echo "Starting the content replayer..."
+        # word split $SWH_EXTRA_CLI_OPTIONS explicitly
+        # shellcheck disable=SC2206
+        CMD=("swh" $SWH_EXTRA_CLI_OPTIONS "objstorage" "replay" "${OPTIONS[@]}" "$@")
+        echo "${CMD[@]}"
+
+        exec "${CMD[@]}"
+        ;;
+esac
