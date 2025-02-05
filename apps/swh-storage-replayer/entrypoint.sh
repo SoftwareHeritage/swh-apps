ac6a8ca1172b7c1745c618962a852c785e8ebe94
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-02-05 15:40:38 +0100

    storage-replayer/entrypoint: Allow to run the image
    This also aligns the entrypoint with the others.

--- apps/swh-storage-replayer/entrypoint.sh
+++ apps/swh-storage-replayer/entrypoint.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-if [ -z "${SWH_CONFIG_FILENAME}" ]; then
-  echo "The SWH_CONFIG_FILENAME environment variable must be set"
-  exit 1
-fi
+set -e
 
-if [ ! -e "${SWH_CONFIG_FILENAME}" ]; then
-  echo "The config file ${SWH_CONFIG_FILENAME} does not exist"
-  exit 1
-fi
+case "$1" in
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
+        fi
+        exit 0
+        ;;
+    *)
+        if [ -z "${SWH_CONFIG_FILENAME}" ]; then
+            echo "The SWH_CONFIG_FILENAME environment variable must be set"
+            exit 1
+        fi
 
-# start the replayer
-echo "Starting the replayer..."
-exec swh storage replay
+        if [ ! -e "${SWH_CONFIG_FILENAME}" ]; then
+            echo "The config file ${SWH_CONFIG_FILENAME} does not exist"
+            exit 1
+        fi
+
+        # start the replayer
+        echo "Starting the replayer..."
+        exec swh storage replay
+        ;;
+esac
