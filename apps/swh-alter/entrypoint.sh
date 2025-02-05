5161031390b1730876bd1458d1dd1ef33cc8ed25
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-02-05 16:01:48 +0100

    alter/entrypoint: Allow to run the image
    This also aligns the entrypoint with the others.

--- apps/swh-alter/entrypoint.sh
+++ apps/swh-alter/entrypoint.sh
@@ -2,10 +2,23 @@
 
 set -e
 
-echo "swh-alter pod: running a tmux server"
-tmux set -g exit-empty off \; start
+case "$1" in
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
+        fi
+        exit 0
+        ;;
+    *)
+        echo "swh-alter pod: running a tmux server"
+        tmux set -g exit-empty off \; start
 
-trap "tmux kill-server" INT TERM EXIT
+        trap "tmux kill-server" INT TERM EXIT
 
-while tmux list-sessions 2>/dev/null; do sleep 5; done
-echo "tmux server has exited, bailing"
+        while tmux list-sessions 2>/dev/null; do sleep 5; done
+        echo "tmux server has exited, bailing"
+        ;;
+esac
