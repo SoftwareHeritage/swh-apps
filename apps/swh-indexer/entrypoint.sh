c9b5c0664e80153c6ad279c1c6ac060b30e27512
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-02-05 16:22:42 +0100

    indexer/entrypoint: Allow to run the image
    This also aligns the entrypoint with the others.

--- apps/swh-indexer/entrypoint.sh
+++ apps/swh-indexer/entrypoint.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-if [ -z "${SWH_CONFIG_FILENAME}" ]; then
-  echo "The SWH_CONFIG_FILENAME environment variable must be set"
-  exit 1
-fi
+set -e
 
-if [ -z "${SWH_INDEXER_TYPE}" ]; then
-  echo "The SWH_INDEXER_TYPE environment variable must be set"
-  exit 1
-fi
+case "$1" in
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
+        fi
+        exit 0
+        ;;
+    *)
+        if [ -z "${SWH_INDEXER_TYPE}" ]; then
+            echo "The SWH_INDEXER_TYPE environment variable must be set"
+            exit 1
+        fi
 
-if [ ! -e "${SWH_CONFIG_FILENAME}" ]; then
-  echo "The config file ${SWH_CONFIG_FILENAME} does not exist"
-  exit 1
-fi
+        if [ ! -e "${SWH_CONFIG_FILENAME}" ]; then
+            echo "The config file ${SWH_CONFIG_FILENAME} does not exist"
+            exit 1
+        fi
 
-# start the replayer
-echo "Starting indexer journal client..."
-exec swh \
-  --log-level azure.core.pipeline.policies.http_logging_policy:WARNING \
-  indexer \
-  --config-file $SWH_CONFIG_FILENAME \
-  journal-client $SWH_INDEXER_TYPE
+        # start the replayer
+        echo "Starting indexer journal client..."
+        exec swh \
+             --log-level azure.core.pipeline.policies.http_logging_policy:WARNING \
+             indexer \
+             --config-file $SWH_CONFIG_FILENAME \
+             journal-client $SWH_INDEXER_TYPE
+        ;;
+esac
