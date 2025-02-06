4abe23d5331f294b0c7c933a8fd2a2413678cf10
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-02-06 18:34:48 +0100

    provenance/entrypoint: Populate cli with flags only when env var is defined
    Refs. swh/infra/sysadm-environment#5560

--- apps/swh-provenance/entrypoint.sh
+++ apps/swh-provenance/entrypoint.sh
@@ -17,20 +17,25 @@ case "$1" in
         exec swh $@
         ;;
     *)
-        EXTRA_CLI_FLAGS=""
-        if [ ! -z "${SWH_LOG_CONFIG_JSON}" ]; then
-            EXTRA_CLI_FLAGS="--log-config-json ${SWH_LOG_CONFIG_JSON}"
+        EXTRA_CLI_FLAGS=()
+        if [ -n "${SWH_LOG_CONFIG_JSON}" ]; then
+            EXTRA_CLI_FLAGS+=('--log-config-json' "${SWH_LOG_CONFIG_JSON}")
         fi
-        echo Starting the swh-provenance RPC server
-        exec gunicorn --bind 0.0.0.0:${PORT} \
-             --log-level ${SWH_LOG_LEVEL:-INFO} \
-             $EXTRA_CLI_FLAGS \
-             --threads ${THREADS} \
-             --workers ${WORKERS} \
-             --timeout ${TIMEOUT} \
+        if [ -n "${STATSD_HOST}" -a -n "${STATSD_PORT}" ]; then
+            EXTRA_CLI_FLAGS+=('--statsd-host' "${STATSD_HOST}:${STATSD_PORT}")
+        fi
+        if [ -n "${STATSD_SERVICE_TYPE}" ]; then
+            EXTRA_CLI_FLAGS+=('--statsd-prefix' "${STATSD_SERVICE_TYPE}")
+        fi
+
+        echo 'Starting the swh-provenance RPC server'
+        exec gunicorn --bind "0.0.0.0:${PORT}" \
+             --log-level "${SWH_LOG_LEVEL:-INFO}" \
+             "${EXTRA_CLI_FLAGS[@]}" \
+             --threads "${THREADS}" \
+             --workers "${WORKERS}" \
+             --timeout "${TIMEOUT}" \
              --reload \
-             --statsd-host=${STATSD_HOST}:${STATSD_PORT} \
-             --statsd-prefix=${STATSD_SERVICE_TYPE} \
              --config 'python:swh.core.api.gunicorn_config' \
              'swh.provenance.api.server:make_app_from_configfile()'
       ;;
