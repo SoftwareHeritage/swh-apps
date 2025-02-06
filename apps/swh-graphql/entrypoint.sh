cd73c7e396fb3260e6465d43e79ff267275c38c3
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-02-06 18:11:58 +0100

    graphql/entrypoint: Populate cli with flags only when env var is defined
    Refs. swh/infra/sysadm-environment#5560

--- apps/swh-graphql/entrypoint.sh
+++ apps/swh-graphql/entrypoint.sh
@@ -12,19 +12,24 @@ case "$1" in
         fi
         ;;
     *)
-        EXTRA_CLI_FLAGS=""
-        if [ ! -z "${SWH_LOG_CONFIG_JSON}" ]; then
-            EXTRA_CLI_FLAGS="--log-config-json ${SWH_LOG_CONFIG_JSON}"
+        EXTRA_CLI_FLAGS=()
+        if [ -n "${SWH_LOG_CONFIG_JSON}" ]; then
+            EXTRA_CLI_FLAGS+=('--log-config-json' "${SWH_LOG_CONFIG_JSON}")
         fi
-        echo Starting the swh-graphql API server
-        exec gunicorn --bind 0.0.0.0:${PORT} \
-             --log-level ${SWH_LOG_LEVEL:-INFO} \
-             ${EXTRA_CLI_FLAGS} \
-             --threads ${THREADS} \
-             --workers ${WORKERS} \
-             --timeout ${TIMEOUT} \
-             --statsd-host=${STATSD_HOST}:${STATSD_PORT} \
-             --statsd-prefix=${STATSD_SERVICE_TYPE} \
+        if [ -n "${STATSD_HOST}" -a -n "${STATSD_PORT}" ]; then
+            EXTRA_CLI_FLAGS+=('--statsd-host' "${STATSD_HOST}:${STATSD_PORT}")
+        fi
+        if [ -n "${STATSD_SERVICE_TYPE}" ]; then
+            EXTRA_CLI_FLAGS+=('--statsd-prefix' "${STATSD_SERVICE_TYPE}")
+        fi
+
+        echo 'Starting the swh-graphql API server'
+        exec gunicorn --bind "0.0.0.0:${PORT}" \
+             --log-level "${SWH_LOG_LEVEL:-INFO}" \
+             "${EXTRA_CLI_FLAGS[@]}" \
+             --threads "${THREADS}" \
+             --workers "${WORKERS}" \
+             --timeout "${TIMEOUT}" \
              --config 'python:swh.graphql.gunicorn_config' \
              --worker-class 'uvicorn.workers.UvicornWorker' \
              'swh.graphql.server:make_app_from_configfile()'
