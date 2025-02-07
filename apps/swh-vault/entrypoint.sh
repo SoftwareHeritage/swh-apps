f6d675e4e07aca18592615779b15886f634941c2
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-02-07 11:30:46 +0100

    vault/entrypoint: Populate cli with flags only when env var is defined
    Refs. swh/infra/sysadm-environment#5560

--- apps/swh-vault/entrypoint.sh
+++ apps/swh-vault/entrypoint.sh
@@ -17,19 +17,24 @@ case "$1" in
         exec swh $@
         ;;
     *)
-        EXTRA_CLI_FLAGS=""
-        if [ ! -z "${SWH_LOG_CONFIG_JSON}" ]; then
-            EXTRA_CLI_FLAGS="--log-config-json ${SWH_LOG_CONFIG_JSON}"
+        EXTRA_CLI_FLAGS=()
+        if [ -n "${SWH_LOG_CONFIG_JSON}" ]; then
+            EXTRA_CLI_FLAGS+=('--log-config-json' "${SWH_LOG_CONFIG_JSON}")
         fi
-        echo Starting the swh-vault API server
-        exec gunicorn --bind 0.0.0.0:${PORT} \
-             --log-level ${SWH_LOG_LEVEL:-INFO} \
-             ${EXTRA_CLI_FLAGS} \
-             --threads ${THREADS} \
-             --workers ${WORKERS} \
-             --timeout ${TIMEOUT} \
-             --statsd-host=${STATSD_HOST}:${STATSD_PORT} \
-             --statsd-prefix=${STATSD_SERVICE_TYPE} \
+        if [ -n "${STATSD_HOST}" -a -n "${STATSD_PORT}" ]; then
+            EXTRA_CLI_FLAGS+=('--statsd-host' "${STATSD_HOST}:${STATSD_PORT}")
+        fi
+        if [ -n "${STATSD_SERVICE_TYPE}" ]; then
+            EXTRA_CLI_FLAGS+=('--statsd-prefix' "${STATSD_SERVICE_TYPE}")
+        fi
+
+        echo 'Starting the swh-vault API server'
+        exec gunicorn --bind "0.0.0.0:${PORT}" \
+             --log-level "${SWH_LOG_LEVEL:-INFO}" \
+             "${EXTRA_CLI_FLAGS[@]}" \
+             --threads "${THREADS}" \
+             --workers "${WORKERS}" \
+             --timeout "${TIMEOUT}" \
              --config 'python:swh.core.api.gunicorn_config' \
              'swh.vault.api.server:make_app_from_configfile()'
         ;;
