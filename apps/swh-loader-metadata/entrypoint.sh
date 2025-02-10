7fa6db1906e28f2ff43555210716a8eb5b8d91a7
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-02-10 16:48:49 +0100

    loader-metadata: Unify entrypoint

--- apps/swh-loader-metadata/entrypoint.sh
+++ apps/swh-loader-metadata/entrypoint.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-if [ -z "${SWH_CONFIG_FILENAME}" ]; then
-  echo "The SWH_CONFIG_FILENAME environment variable must be set"
-  exit 1
-fi
+case "$1" in
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
+        fi
+        ;;
+    *)
 
-if [ ! -e "${SWH_CONFIG_FILENAME}" ]; then
-  echo "The config file ${SWH_CONFIG_FILENAME} does not exist"
-  exit 1
-fi
+        if [ -z "${SWH_CONFIG_FILENAME}" ]; then
+            echo "The SWH_CONFIG_FILENAME environment variable must be set"
+            exit 1
+        fi
 
-echo "Starting loader metadata journal client..."
-exec swh \
-  metadata-loader \
-  --config-file $SWH_CONFIG_FILENAME \
-  journal-client $SWH_INDEXER_TYPE
+        if [ ! -e "${SWH_CONFIG_FILENAME}" ]; then
+            echo "The config file ${SWH_CONFIG_FILENAME} does not exist"
+            exit 1
+        fi
+
+        echo "Starting loader metadata journal client..."
+        exec swh \
+             metadata-loader \
+             --config-file $SWH_CONFIG_FILENAME \
+             journal-client $SWH_INDEXER_TYPE
+        ;;
+esac
