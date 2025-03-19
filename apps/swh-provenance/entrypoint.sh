9d71df1e3b7a8e25f242fc68f6feda68039b89f7
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-03-19 12:41:43 +0100

    provenance/entrypoint.sh: Adapt to run either a grpc or rpc
    Refs. swh/infra/sysadm-environment#5608

--- apps/swh-provenance/entrypoint.sh
+++ apps/swh-provenance/entrypoint.sh
@@ -18,25 +18,40 @@ case "$1" in
         ;;
     *)
         EXTRA_CLI_FLAGS=()
-        if [ -n "${SWH_LOG_CONFIG_JSON}" ]; then
-            EXTRA_CLI_FLAGS+=('--log-config-json' "${SWH_LOG_CONFIG_JSON}")
-        fi
         if [ -n "${STATSD_HOST}" -a -n "${STATSD_PORT}" ]; then
             EXTRA_CLI_FLAGS+=('--statsd-host' "${STATSD_HOST}:${STATSD_PORT}")
         fi
-        if [ -n "${STATSD_SERVICE_TYPE}" ]; then
-            EXTRA_CLI_FLAGS+=('--statsd-prefix' "${STATSD_SERVICE_TYPE}")
-        fi
+        case "${PROVENANCE_TYPE}" in
+            "rpc")
+                if [ -n "${SWH_LOG_CONFIG_JSON}" ]; then
+                    EXTRA_CLI_FLAGS+=('--log-config-json' "${SWH_LOG_CONFIG_JSON}")
+                fi
+                if [ -n "${STATSD_SERVICE_TYPE}" ]; then
+                    EXTRA_CLI_FLAGS+=('--statsd-prefix' "${STATSD_SERVICE_TYPE}")
+                fi
 
-        echo 'Starting the swh-provenance RPC server'
-        exec gunicorn --bind "0.0.0.0:${PORT}" \
-             --log-level "${SWH_LOG_LEVEL:-INFO}" \
-             "${EXTRA_CLI_FLAGS[@]}" \
-             --threads "${THREADS}" \
-             --workers "${WORKERS}" \
-             --timeout "${TIMEOUT}" \
-             --reload \
-             --config 'python:swh.core.api.gunicorn_config' \
-             'swh.provenance.api.server:make_app_from_configfile()'
-      ;;
+                echo 'Starting the swh-provenance RPC server'
+                exec gunicorn --bind "0.0.0.0:${PORT}" \
+                     --log-level "${SWH_LOG_LEVEL:-INFO}" \
+                     "${EXTRA_CLI_FLAGS[@]}" \
+                     --threads "${THREADS}" \
+                     --workers "${WORKERS}" \
+                     --timeout "${TIMEOUT}" \
+                     --reload \
+                     --config 'python:swh.core.api.gunicorn_config' \
+                     'swh.provenance.api.server:make_app_from_configfile()'
+              ;;
+            "grpc")
+                echo 'Starting the swh-provenance GRPC server'
+                exec swh-provenance-grpc-serve \
+                    --graph "${GRAPH_PATH}" \
+                    --database "${PROVENANCE_PATH}" \
+                    --bind ":${PORT}"
+                    "${EXTRA_CLI_FLAGS[@]}" \
+                ;;
+
+            *)
+                echo "Unknown provenance type <${PROVENANCE_TYPE}> (either rpc or gprc)"
+                exit 1
+        esac
 esac
