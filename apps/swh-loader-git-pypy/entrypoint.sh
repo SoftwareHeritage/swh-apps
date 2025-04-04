dd5c7e879efbd565f15005e5f4a0141be8ea77ff
Nicolas Dandrimont <nicolas@dandrimont.eu>
2025-04-04 16:18:48 +0200

    Introduce pypy-based image for the git loader

---
+++ apps/swh-loader-git-pypy/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+case "$1" in
+    "shell")
+        shift
+        if (( $# == 0)); then
+            exec bash -i
+        else
+            "$@"
+        fi
+        ;;
+    *)
+        # If a pod is killed by the OOM killer, there can remain previous
+        # workdir as the ephemeral volumes are not recreated
+        echo "Cleaning /tmp..."
+        rm -rf /tmp/*
+
+        echo Starting the swh Celery worker for ${SWH_WORKER_INSTANCE}
+        exec python -m celery \
+             --app=swh.scheduler.celery_backend.config.app \
+             worker \
+             --pool=prefork \
+             --concurrency=${CONCURRENCY} \
+             --max-tasks-per-child=${MAX_TASKS_PER_CHILD} \
+             -Ofair \
+             --loglevel=${SWH_LOG_LEVEL:-INFO} \
+             --without-gossip --without-mingle --without-heartbeat \
+             --hostname "${SWH_WORKER_INSTANCE}@%h"
+        ;;
+esac
