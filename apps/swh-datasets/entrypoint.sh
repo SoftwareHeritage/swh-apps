2135d7f59da64558aa9c63b134220703f8f140d8
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2025-04-05 12:32:28 +0200

    swh-datasets: Allo luigi scheduler usage
    Refs. swh/infra/sysadm-environment#5623

--- apps/swh-datasets/entrypoint.sh
+++ apps/swh-datasets/entrypoint.sh
@@ -12,20 +12,22 @@ case "$1" in
         fi
         exit 0
         ;;
-    "swh")
-        shift
-        echo "Running swh command $@"
-        exec swh $@
-        ;;
-    "luigi")
-        shift
-        echo "Running luigid scheduler"
-        exec luigid --background \
-             --pidfile "${LUIGI_PATH_TO_PIDFILE}" \
-             --logdir "${LUIGI_PATH_TO_LOGDIR}" \
-             --state-path "${LUIGI_PATH_TO_STATEFILE}"
-        ;;
     *)
-        echo Unknown command $@
-        exit 1
+        case "${APP_NAME}" in
+            "swh")
+                shift
+                echo "Running swh command $@"
+                exec swh "$@"
+                ;;
+            "luigi")
+                echo "Running luigid scheduler"
+                exec luigid --background \
+                     --pidfile "${LUIGI_PATH_TO_PIDFILE}" \
+                     --logdir "${LUIGI_PATH_TO_LOGDIR}" \
+                     --state-path "${LUIGI_PATH_TO_STATEFILE}"
+                ;;
+            *)
+                echo Unknown command $@
+                exit 1
+        esac
 esac
